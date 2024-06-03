f8689457b04e3c3740c11b107369fda06b3c8a0a
fawadmalik <fawad.malik@hotmail.com>
2024-06-03 00:11:53 -0700

    Adds test setup code to verify that tests can rely on webdrivermanager for chrome

--- PurchaseSuccessTests.java
+++ PurchaseSuccessTests.java
@@ -1,19 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
 
-public class PurchaseSuccessTests{
+public class PurchaseSuccessTests {
     private WebDriver driver;
 
     @BeforeMethod
     public void testSetup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
-    public void testTeardown(){}
-    
+    public void testTeardown(){
+        driver.quit();
+    }
+
     @Test
-    public void createDiscountWithoutInterfaces() {
-    } 
+    public void verifyTestConfigurationSetup(){
+        driver.navigate().to("http://demos.bellatrix.solutions/");
+        System.out.println("Title: " + driver.getTitle());
+    }
 }
