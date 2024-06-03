7788508dcad616cc43e6bae1d716cee756161000
fawadmalik <fawad.malik@hotmail.com>
2024-06-02 23:57:51 -0700

    Adds placeholder methods for test method level setup and teardown

--- PurchaseSuccessTests.java
+++ PurchaseSuccessTests.java
@@ -1,6 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PurchaseSuccessTests{
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void testSetup(){
+    }
+
+    @AfterMethod
+    public void testTeardown(){}
+    
     @Test
     public void createDiscountWithoutInterfaces() {
     } 
