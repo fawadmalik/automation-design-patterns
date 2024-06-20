dd3338b00508283a7336ae68980b7b91754f50e3
Fawad Malik <fawad.malik@hotmail.com>
2024-06-19 22:54:05 -0700

    Implements element decorator class that implements logging

---
+++ src/test/java/org/fmalik/automation/decoratorPatternImplementation/v3/LogElement.java
@@ -0,0 +1,36 @@
+package org.fmalik.automation.decoratorPatternImplementation.v3;
+
+import org.openqa.selenium.By;
+
+public class LogElement extends ElementDecorator{
+    public LogElement(Element element) {
+        super(element);
+    }
+    
+    @Override
+    public String getText(){
+        System.out.println(String.format("Element Text=%s", element.getText()));
+        return element.getText();
+    }
+
+    @Override
+    public Boolean isEnabled(){
+        System.out.println(String.format("Element Enabled=%b", element.isEnabled()));
+        return element.isEnabled();
+    }
+
+    @Override
+    public Boolean isDisplayed(){
+        System.out.println(String.format("Element Displayed=%b", element.isEnabled()));
+        return element.isDisplayed();
+    }    @Override
+    public void typeText(String text){
+        System.out.println(String.format("Type Text==%s", text));
+        element.typeText(text);
+    }
+    @Override
+    public String getAttribute(String attributeName){
+        System.out.println(String.format("Element attribute %s=%s", attributeName, getAttribute(attributeName)));
+        return element.getAttribute(attributeName);
+    }
+}
